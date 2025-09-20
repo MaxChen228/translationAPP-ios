ac4b11c8cbbad93d44e9819f3ccb5828ba181ecb
Max0228 <max970228@gmail.com>
2025-09-21 07:14:23 +0800

    refactor: extract flashcard models

--- ios/translation/Features/Flashcards/Views/FlashcardsView.swift
+++ ios/translation/Features/Flashcards/Views/FlashcardsView.swift
@@ -1,21 +1,5 @@
 import SwiftUI
 
-struct Flashcard: Identifiable, Codable, Equatable {
-    let id: UUID
-    var front: String
-    var frontNote: String?
-    var back: String
-    var backNote: String?
-
-    init(id: UUID = UUID(), front: String, back: String, frontNote: String? = nil, backNote: String? = nil) {
-        self.id = id
-        self.front = front
-        self.frontNote = frontNote
-        self.back = back
-        self.backNote = backNote
-    }
-}
-
 final class FlashcardsStore: ObservableObject {
     @Published var cards: [Flashcard]
     @Published var index: Int = 0
@@ -55,25 +39,6 @@ final class FlashcardsStore: ObservableObject {
     ]
 
     // End of store helpers
-}
-
-private enum AnnotateFeedback: Equatable {
-    case familiar
-    case unfamiliar
-
-    var color: Color {
-        switch self {
-        case .familiar: return DS.Palette.success
-        case .unfamiliar: return DS.Palette.warning
-        }
-    }
-
-    var label: LocalizedStringKey {
-        switch self {
-        case .familiar: return "flashcards.annotate.familiar"
-        case .unfamiliar: return "flashcards.annotate.unfamiliar"
-        }
-    }
 }
 
 struct FlashcardsView: View {
